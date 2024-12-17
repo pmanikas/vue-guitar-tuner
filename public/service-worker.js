e31affcdac8deadc53a97ba850dd545b41ff7098
Pandelis Manikas <pmanikas.social@gmail.com>
2024-12-17 13:19:23 +0200

    Complete the PWA setup

---
+++ public/service-worker.js
@@ -0,0 +1,43 @@
+const CACHE_VERSION = 1;
+const CACHE_NAME = `guitar-tuner-app-cache-${CACHE_VERSION}`;
+const urlsToCache = [
+    "/",
+    "/index.html",
+    "/icons/android-chrome-192x192.png",
+    "/icons/android-chrome-512x512.png",
+    "/icons/apple-touch-icon.png",
+    "/icons/favicon-16x16.png",
+    "/icons/favicon-32x32.png",
+    "/icons/favicon.ico",
+];
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener("activate", (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,18 @@
-import { createApp } from 'vue';
-import './styles/style.scss';
-import App from './App.vue';
+import { createApp } from "vue";
+import "./styles/style.scss";
+import App from "./App.vue";
 
-createApp(App).mount('#app');
+createApp(App).mount("#app");
+
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+        navigator.serviceWorker.register("/service-worker.js").then(
+            (registration) => {
+                console.log("Service Worker registered with scope:", registration.scope);
+            },
+            (err) => {
+                console.error("Service Worker registration failed:", err);
+            }
+        );
+    });
+}
