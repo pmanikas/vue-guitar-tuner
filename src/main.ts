fef6a74ac78a3ed3c516182cb5cd0bc6bdc54dde
Pandelis Manikas <pmanikas.social@gmail.com>
2024-12-18 13:22:51 +0200

    Fix paths

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ createApp(App).mount("#app");
 
 if ("serviceWorker" in navigator) {
     window.addEventListener("load", () => {
-        navigator.serviceWorker.register("/service-worker.js").then(
+        navigator.serviceWorker.register("service-worker.js").then(
             (registration) => {
                 console.log("Service Worker registered with scope:", registration.scope);
             },
